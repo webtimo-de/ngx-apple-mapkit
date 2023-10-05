65ba807a120713bd2e74a4598d346da8715850fa
Timo Köhler <timok@web-timo.de>
2023-10-05 16:38:48 +0200

    0.0.19

--- public-api.d.ts
+++ public-api.d.ts
@@ -4,3 +4,4 @@ export * from './lib/services/search/apple-maps-search.service';
 export * from './lib/apple-mapkit.component';
 export * from './lib/components/apple-mapkit-annotation/apple-mapkit-annotation.component';
 export * from './lib/apple-maps.module';
+export * from './lib/mapkit';
